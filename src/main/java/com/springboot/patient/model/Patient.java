ee12376c8b84e53ffdcaa14b21a754d7ad3a6f81
miaaaking <jinie128@gmail.com>
2023-02-12 22:36:12 +0900

    요구 사항 완료

--- src/main/java/com/springboot/patient/model/Patient.java
+++ src/main/java/com/springboot/patient/model/Patient.java
@@ -14,20 +14,6 @@ public class Patient {
 
     private String img_url;
 
-
-    public String getRandomCode(int length) {
-        String alphaNum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        int alphaNumLength = alphaNum.length();
-
-        Random random = new Random();
-
-        StringBuffer code = new StringBuffer();
-        for (int i = 0; i < length; i++) {
-            code.append(alphaNum.charAt(random.nextInt(alphaNumLength)));
-        }
-
-        return code.toString();
-    }
     public Patient(String patient_name, String patient_age, String patient_sex, String disease_yn, String img_url) {
         this.patient_name = patient_name;
         this.patient_age = patient_age;
